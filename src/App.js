18338609c0cdfa37c05487483c066b38a27cd0a9
Nwankwo Emmanuel <emmatexiii@gmail.com>
2024-05-21 14:48:54 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import Nav from "./retaurants/Nav"
+import {Main} from "./retaurants/Main"
+
+import { Footer } from "./retaurants/Footer"
+
+function App() {
+  return (
+    <div>
+      <Nav/>
+      <Main/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/retaurants/Inspiration.js
@@ -0,0 +1,23 @@
+const Inspiration = ({ check, calfunc }) => {
+    calfunc(20)
+  return (
+    <div className="card">
+      <div className="card-header">Inspirational Quotes...</div>
+      <div className="card-body">
+        <h3>Get Motivated and Get Moving with the Quotes Below...</h3>
+        <ul>
+          {check.map(function (quo,index) {
+            //we can loop wih this
+              return <li key={index}>{quo.text}***|<span>{quo.author}</span></li>;
+          })}
+              </ul>
+              
+      <button onClick={()=>{alert(calfunc(8))}}>Click Me</button>
+
+      </div>
+      </div>
+      
+  );
+};
+
+export default Inspiration;
